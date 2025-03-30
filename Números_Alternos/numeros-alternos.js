3164bf3d415b1c2badeda9eb08671986a6c1d95c
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-30 00:01:24 -0500

    numeros alternos

--- Números_Alternos/numeros-alternos.js
+++ Números_Alternos/numeros-alternos.js
@@ -8,11 +8,26 @@ Si el número es impar, imprímelo en orden inverso (ejemplo: 13 se imprime como
 Nota: Si el número impar tiene solo un dígito, se imprime igual.
 */
 
+let i = 0;
 
+while (i <= 50) {
+  if (i % 2 == 0) {
+    console.log(`${i} Es par`);
+  } else if (i % 1 == 0) {
+    let reverse = i.toString().split("").reverse().join("");
+    console.log(`${reverse} es impar`);
+  } else {
+    console.log(i);
+  }
 
+  i++;
+}
 
+// while(i<10){
 
-
-
-
-
+//     if (i % 2 == 0) {
+//     let reverse = i.toString().split("").reverse().join("");
+//     console.log(reverse);
+//     }
+//     i++
+// }
