ebc25ad913a65e3c01884fda75ef7f4fc0072883
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-29 23:22:22 -0500

    numeros alternos ejercicio

---
+++ Números_Alternos/numeros-alternos.js
@@ -0,0 +1,18 @@
+/*
+Escribe un programa que imprima los números del 1 al 50, pero con el siguiente patrón:
+
+Si el número es par, imprímelo en formato normal.
+
+Si el número es impar, imprímelo en orden inverso (ejemplo: 13 se imprime como 31).
+
+Nota: Si el número impar tiene solo un dígito, se imprime igual.
+*/
+
+
+
+
+
+
+
+
+
