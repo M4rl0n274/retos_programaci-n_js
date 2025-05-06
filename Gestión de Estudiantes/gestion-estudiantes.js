6ba527c35820f63bf3738391c9ed5dd1312edd89
M4rl0n274 <marlonquinteri274@gmail.com>
2025-05-05 23:24:35 -0500

    gestion estudiantes

---
+++ Gestión de Estudiantes/gestion-estudiantes.js
@@ -0,0 +1,20 @@
+// Crea un programa en JavaScript que permita gestionar un grupo de estudiantes. Cada estudiante debe ser representado como un objeto con las siguientes propiedades:
+// nombre (string)
+// edad (number)
+// notas (arreglo de números)
+// activo (booleano)
+// Tu programa debe hacer lo siguiente:
+// Crear un arreglo de al menos 5 estudiantes con datos de ejemplo.
+// Recorrer el arreglo usando un bucle para:
+// Mostrar el nombre y promedio de notas de cada estudiante.
+// Indicar si el estudiante aprueba (promedio ≥ 3.0) o reprueba.
+// Contar cuántos estudiantes están activos.
+// Mostrar el estudiante con el mejor promedio.
+// Crear una función que reciba el nombre de un estudiante y devuelva toda su información si existe.
+// Agregar un nuevo estudiante al arreglo usando una función.
+// Eliminar a los estudiantes que están inactivos del arreglo final.
+
+
+
+
+
